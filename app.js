d488928aa7c6ab916d8cc6df3cc242dd061d240b
Francis Calizo <francisacalizo@gmail.com>
2019-07-16 22:50:16 -0400

    Add Success and Error Alerts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('book-form');
+const alert = document.getElementById('alert');
 
 // Book Constructor
 function Book(title, author, isbn) {
@@ -14,6 +15,20 @@ Book.prototype.addNewBook = function() {
   const titleCell = row.insertCell(0);
   const authorCell = row.insertCell(1);
   const isbnCell = row.insertCell(2);
+  const deleteCell = row.insertCell(3);
+
+  titleCell.textContent = this.title;
+  authorCell.textContent = this.author;
+  isbnCell.textContent = this.isbn;
+  deleteCell.innerHTML = `<a href="#">X</a>`;
+
+  // Display Success Alert and Remove after 3 Seconds
+  alert.textContent = 'Book Added';
+  alert.className = 'success';
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove('success');
+  }, 3000);
 };
 
 form.addEventListener('submit', e => {
@@ -21,8 +36,24 @@ form.addEventListener('submit', e => {
   const author = document.getElementById('author');
   const isbn = document.getElementById('isbn');
 
-  const newBook = new Book(title.value, author.value, isbn.value);
+  if (title.value != '' && author.value != '' && isbn.value != '') {
+    // Instantiate New Book
+    const newBook = new Book(title.value, author.value, isbn.value);
 
-  console.log(newBook.addNewBook());
+    // Add New Book to List
+    newBook.addNewBook();
+    title.value = '';
+    author.value = '';
+    isbn.value = '';
+  } else {
+    // throw error alert
+    alert.textContent = 'Please check your input fields.';
+    alert.className = 'error';
+    setTimeout(() => {
+      alert.textContent = '';
+      alert.classList.remove('error');
+    }, 3000);
+  }
+
   e.preventDefault();
 });
